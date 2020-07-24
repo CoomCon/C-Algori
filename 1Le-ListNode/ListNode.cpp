0a824879a98a8e9c53278e22ca94c44d4efc99f2
Meng Kou <koumenghust@gmail.com>
2020-07-24 16:28:38 +0800

    20200724 BigDataMul

--- 1Le-ListNode/ListNode.cpp
+++ 1Le-ListNode/ListNode.cpp
@@ -13,8 +13,30 @@ typedef struct ListNode {
 //一般定义头指针和尾指针方便处理
 pListNode g_pHead = NULL; //等效于 ListNode  *g_pHead = NULL;
 ListNode *g_pEnd = NULL;
+// 头插法 头插法一般在大数相乘 大数幂的情景用的比较多
+// 大数幂时，结果是反过来存储的，高位存储在低位节点上 以后再说
+void adddataFront(int a)
+{
+    //1。 创建一个节点
+    pListNode ptemp = (pListNode)malloc(sizeof(pListNode));
+    ptemp->data = a;
+    ptemp->next = NULL;
+    if(g_pHead==NULL)
+    {
+        g_pHead = ptemp;
+        g_pEnd = ptemp;
+    }else //将头链接进去
+    {
+        ptemp->next = g_pHead;
+    }
+    g_pHead = ptemp;
+
+}
 
 
+
+
+//尾插法
 void adddata(int a)
 {
 	//1.创建一个节点
@@ -90,12 +112,12 @@ pListNode reverseK(pListNode head, int k)
 int main()
 {
     g_pHead;
-	 adddata(1);
-	 adddata(2);
-	 adddata(3);
-	 adddata(4);
-     adddata(5);
-     adddata(6);
+	 adddataFront(1);
+	 adddataFront(2);
+	 adddataFront(3);
+	 adddataFront(4);
+     adddataFront(5);
+     adddataFront(6);
     // pListNode ret =  reverse(g_pHead);
      pListNode reret = reverseK( g_pHead, 3);
      //Solution so;
--- dashum/dashumi.cpp
+++ dashum/dashumi.cpp
@@ -1,7 +1,12 @@
-#include   <iostream>
+#include <iostream>
 #include <vector>
+#include <sstream>
+#include <string>
+#include <stack>
+
 using namespace std;
 
+//快速幂
 int poww(int a, int b){ // return a ^ b
     int ans = 1, base = a;
     while(b != 0){
@@ -13,55 +18,151 @@ int poww(int a, int b){ // return a ^ b
     return ans;
 }
 
+// #define MAX 1000000
+// int nbit;
+// int ans[MAX];
 
-vector<int> powtemp(int n,int m) //n^m
-{
-    vector<int> res;
-    int weishu = 0;
-    int temp=n;
-    for (int i = 1; i < m;i++)
-    {
-        temp *= n;
 
-        if(temp % 10 > 0)
-        {
-            
-        }
-    }
-    return res;
-}
-
-#define MAX 1000000
-int nbit;
-int ans[MAX];
+// void init(){
+//     nbit = 1;
+//     ans[0] = 1;
+// }
  
-void init(){
-    nbit = 1;
-    ans[0] = 1;
-}
- 
-
-
+vector<int> ans; 
+// 大数幂 的基础 大数乘法
 void mul(int num){
     int temp_add = 0;
     int temp_mul = 0;
-    for(int i = 0; i < nbit; i++){
+    for(int i = 0; i < ans.size(); i++){
         temp_mul = num * ans[i];
         ans[i] = (temp_mul + temp_add) % 10;
         temp_add = (temp_mul + temp_add) / 10;
     }
     while(temp_add){
-        ans[nbit++] = temp_add % 10;
+        ans.push_back( temp_add % 10);
         temp_add /= 10;
     }
 }
 
-void exp(int b, int e){
-    for(int i = 0; i < e; i++) mul(b);
+// int Dsmod(int b,int num,int modnum){
+//     int tempb = b % modnum;
+//     for(int i = 1; i < num; i++)
+//         tempb = tempb * tempb % modnum;
+//     return tempb;
+
+// }
+
+void init(int b)
+{
+    ans.clear();
+    while(b>0)
+    {
+        ans.push_back(b % 10); //倒序
+        b /= 10;
+    }
 }
 
+vector<int> powd(int b, int e){
+    init(b);
+    for(int i = 1; i < e; i++) 
+        mul(b);
+    return ans;
+}
+
+
+vector<int> allo(int b){
+    vector<int> res;
+    while(b>0)
+    {
+        res.push_back(b % 10); //倒序
+        b /= 10;
+    }
+    return res;
+}
+//检查是否是相等
+bool check(vector<int> a,vector<int> b)
+{
+    //可以用堆栈来检查
+  
+    for (vector<int>::iterator iter = b.begin(); iter != b.end();) //itereate
+        {
+            for (vector<int>::iterator iter2 = a.begin(); iter2 != a.end();)
+            {
+            if (*iter == *iter2)
+            {
+                a.erase(iter2);
+                b.erase(iter);
+                iter = b.begin();
+                iter2 = a.begin();
+                break;
+            }else
+            {
+                iter2++;
+            }
+            }
+            
+        }
+    if(a.size()==0)
+        return true;
+    else
+        return false;
+}
+
+
+void outpur(vector<int> inv)
+{
+    for (vector<int>::iterator iter = inv.begin(); iter != inv.end();iter++)
+    {
+        cout << *iter << "-";
+    }
+    cout << endl;
+}
 int main()
 {
-    int a = poww(4, 3);
+
+    int inpu;//有范围
+    //空格分隔的大数
+    // string line;
+    // vector<int> inpu;
+    // getline(cin, line);
+    // stringstream ss(line);
+    // while(ss)
+    // {
+    //     int temp;
+    //     ss >> temp;
+    //     inpu.push_back(temp);
+    // }
+    //init(b)
+    cin >> inpu;
+    vector<int> inve = allo(inpu);
+    vector<int> a2n;
+    vector<int> n;
+    for (int i = 1;;)//1开始 
+    {
+        //可能会涉及到大数
+        ans.clear();
+        a2n = powd(2, i);
+        if(a2n.size()<inve.size())
+            i++;
+        else if (a2n.size()==inve.size())
+        {
+            if(check(a2n,inve))
+            {
+                outpur(inve);
+                n.push_back(i);
+            }
+            
+            i++;
+        }else
+        {
+            break;
+        }
+    }
+    //
+    if(n.size() == 0)
+    {
+        cout << -1 << endl; //没有复合个数的n
+    }
+    system("pause");
     return 0;
 }
